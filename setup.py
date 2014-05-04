7fb1a018c8dc628903dab2c13e1ad70abd3e7b37
Eugene Vereshchagin <evvers@gmail.com>
2014-05-04 17:50:48 +0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='git-pre-commit-hook',
+    version='0.0.1',
+    author='Evgeny Vereshchagin',
+    author_email='evvers@ya.ru',
+    packages=find_packages(),
+)
