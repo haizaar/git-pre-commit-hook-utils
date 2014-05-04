044e893f5be5f233ea6f8029d92c9fd67b2b9981
Eugene Vereshchagin <evvers@gmail.com>
2014-05-04 18:10:53 +0400

    Change package name to 'git-pre-commit-hook-utils'

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='git-pre-commit-hook',
+    name='git-pre-commit-hook-utils',
     version='0.0.1',
     description='utils for implementing git pre-commit hook',
     author='Evgeny Vereshchagin',
     author_email='evvers@ya.ru',
-    url='https://github.com/evvers/git-pre-commit-hook',
+    url='https://github.com/evvers/git-pre-commit-hook-utils',
     packages=find_packages(),
 )
