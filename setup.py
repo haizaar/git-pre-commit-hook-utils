f1b646e2ffb7692a4223d2c4ecbe06ec9feea629
Eugene Vereshchagin <evvers@gmail.com>
2014-05-04 18:05:36 +0400

    Add description to setup metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='git-pre-commit-hook',
     version='0.0.1',
+    description='utils for implementing git pre-commit hook',
     author='Evgeny Vereshchagin',
     author_email='evvers@ya.ru',
     url='https://github.com/evvers/git-pre-commit-hook',
