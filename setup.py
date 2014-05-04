e63ccaa19aa764fda65abaa85d4c93db2b51fb05
Eugene Vereshchagin <evvers@gmail.com>
2014-05-04 18:22:44 +0400

    Add license to setup metadata

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='git-pre-commit-hook-utils',
     version='0.0.1',
     description='utils for implementing git pre-commit hook',
+    license='MIT',
     author='Evgeny Vereshchagin',
     author_email='evvers@ya.ru',
     url='https://github.com/evvers/git-pre-commit-hook-utils',
