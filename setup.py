745750230037186015e4a316418d83b303e78bc5
Eugene Vereshchagin <evvers@gmail.com>
2014-05-04 17:52:41 +0400

    Add url to setup metadata

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version='0.0.1',
     author='Evgeny Vereshchagin',
     author_email='evvers@ya.ru',
+    url='https://github.com/evvers/git-pre-commit-hook',
     packages=find_packages(),
 )
