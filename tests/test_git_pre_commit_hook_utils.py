89984eb5e9e8cdb8420ff1da07c54ce0dd265629
Eugene Vereshchagin <evvers@gmail.com>
2014-05-15 11:51:40 +0400

    Add test for commit to empty repo case

--- tests/test_git_pre_commit_hook_utils.py
+++ tests/test_git_pre_commit_hook_utils.py
@@ -1,4 +1,29 @@
 import git_pre_commit_hook_utils as utils
+import scripttest
+import os
+import copy
+
+
+def test_with_empty_repo(tmpdir):
+    os_environ = copy.deepcopy(os.environ)
+    os_environ['GIT_DIR'] = str(tmpdir)
+    os_environ['GIT_WORK_TREE'] = str(tmpdir)
+    env = scripttest.TestFileEnvironment(
+        str(tmpdir),
+        start_clear=False,
+        template_path='data',
+        environ=os_environ,
+    )
+    env.writefile('empty_file', content='')
+    env.run('git', 'init')
+    env.run('git', 'add', 'empty_file')
+    files_staged_for_commit = list(utils.files_staged_for_commit())
+    assert len(files_staged_for_commit) == 1
+    file_at_index = files_staged_for_commit[0]
+    assert file_at_index.path == 'empty_file'
+    assert file_at_index.contents == ''
+    assert file_at_index.size == 0
+    assert file_at_index.status == 'A'
 
 
 def test_is_python_code_by_path():
